422b18d45f9d824b1437d4e72ec25871a8f7bb2a
Alexey Alekhin <nihkela@gmail.com>
2021-03-31 17:52:19 +0300

    create new project

---
+++ src/main/java/rest/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package rest.service;
+
+import rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
+}
