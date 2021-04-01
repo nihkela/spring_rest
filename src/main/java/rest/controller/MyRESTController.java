a3dbb44ed3d3c79a781819b0c7d58dbddba16018
Alexey Alekhin <nihkela@gmail.com>
2021-04-01 21:28:01 +0300

    Create deleteEmployee method

--- src/main/java/rest/controller/MyRESTController.java
+++ src/main/java/rest/controller/MyRESTController.java
@@ -41,4 +41,23 @@ public class MyRESTController {
         employeeService.saveEmployee(employee);
         return employee;
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database");
+        }
+
+        employeeService.deleteEmployee(id);
+
+        return "Employee with ID = " + id + " was deleted";
+    }
 }
